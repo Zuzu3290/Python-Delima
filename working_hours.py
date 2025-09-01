89a17768db04306984854155a6918d057e0ed38a
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 23:01:29 +0200

    Create working_hours.py

---
+++ working_hours.py
@@ -0,0 +1,4 @@
+# So I got an idea to replicate an excel sheet and demosntrate the application of workling hours of an individual. Basically a Timesheet. 
+# with functions of how many hours worked with evaulation of input time and stop time
+# with functions of how many hours worked with evaluation of input time and hours worked 
+# this application could be enhanced withthe pomodoro timer schenanigan. 
