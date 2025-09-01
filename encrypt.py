a69389a64570eec681094a95c1c5e0aba13f159a
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 23:34:17 +0200

    Create encrypt.py
    The most exciting project I have in mind

---
+++ encrypt.py
@@ -0,0 +1,2 @@
+#the project of ugly vowel eater gave me a project idea. An encryption algorithm to enforce a messaeg a fuction is called. encrypting the message to whioch another function is called to decrypt it. 
+#The idea is to produce a suitbale encapsulation of real-world data btransfer or messaging or simple wireless hardware communication. 
