f5e359722de2a9b2c679a2ea7af998c84381ab77
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-02 00:00:58 +0200

    Create Sorting_groups.py

---
+++ Sorting_groups.py
@@ -0,0 +1,2 @@
+#The project is to sort users (individuals) into groups. Without allowing baised selection set to adjust people into groups.
+#This project is to use a list conatining lists with the inidiviuals being sorted. AAAA another stupid but learning project. 
