9026cc8280260961ff372fe6688e24b40b8821cd
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 23:30:20 +0200

    Create German_tax_collector.py

---
+++ German_tax_collector.py
@@ -0,0 +1,2 @@
+#I thought of designing a programm that could be used to calculate the german tax collection. Instead of composing everything into my mind a programm that is capable to evaluating the amount tax the government is due to me 
+#or if im unfortunate, Im expected to pay 
