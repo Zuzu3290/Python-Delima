d763ac4a28e589d1ba2b871f7bccad3c0d06da12
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 23:54:46 +0200

    Create Shitty_bribe.py

---
+++ Shitty_bribe.py
@@ -0,0 +1,2 @@
+# I learned list but want to employ a project just with lists. So, a weird project is in mind.
+#Using lists we design a daat list of amount gained from a bribe from students with the students name also listed. A stupid thing but a teacher wants to keep a record of hwo much he gained from wolkring two jobs at once . LOL LOL LOL
