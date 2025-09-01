dd030dd03f29b114ab9feac22527fdb0743dcb59
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 23:06:39 +0200

    Create BEU.py

---
+++ BEU.py
@@ -0,0 +1,2 @@
+# # The complication of visualizing the mechanism of the battery in any body is cruical. So, this program is to apply the evaluation of the battery. The charge due or the percentage left. for basics understanding the underlying application 
+# implementation we will use the comparator and further progress. 
