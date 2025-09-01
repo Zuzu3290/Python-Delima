f86e36b6a94ae87d168e1e7c84d48b8b4765d760
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 22:44:13 +0200

    Create The Library.py
    Case admitted

---
+++ The Library.py
@@ -0,0 +1,3 @@
+#  Project 2 : backend schenanigan  for updating and providiong stats about the library. 
+# How many books are there and what. Updating the location and providing changes 
+# for this delima it would ideally require a database, but trying to model the whole backend with python. Pesudo pseudo
