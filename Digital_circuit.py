d59a2e704655967064eb8990aca179f5356b9cd5
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-01 23:50:08 +0200

    Create Digital_circuit.py

---
+++ Digital_circuit.py
@@ -0,0 +1,4 @@
+#Applying a digital circuitry design into a working pythoin program. 
+#idea is that given the condcutor is driving the train he is to stay active. Instead of just implying  a use of one button we apply the approach of byte based encryption to verify the conductor is alive. 
+# this is a case I concluded but looking to adhere to larger scope of the project. Enforcing a more versatile project to imply a bit, a bit of I guess hardware design using pyton. 
+#Even though we could approach the  program using micropython, But ehh lets try to work withpythona nd then study the sematics of micropython implementation.  
